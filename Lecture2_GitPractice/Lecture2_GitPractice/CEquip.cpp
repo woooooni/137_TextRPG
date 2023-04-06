129685439cc85e1e273a70568cee55edfb3a3f8c
Juns <gmlwnsgmlcks@gmail.com>
2023-04-06 13:05:50 +0900

    Feat.. "장비창"
    test 폴더 같은거

---
+++ Lecture2_GitPractice/Lecture2_GitPractice/CEquip.cpp
@@ -0,0 +1,27 @@
+#include "stdafx.h"
+#include "CEquip.h"
+
+
+CEquip::CEquip()
+{
+}
+
+CEquip::~CEquip()
+{
+}
+
+void CEquip::Initialize()
+{
+}
+
+void CEquip::Update()
+{
+}
+
+void CEquip::Render()
+{
+}
+
+void CEquip::Release()
+{
+}
---
+++ Lecture2_GitPractice/Lecture2_GitPractice/CEquip.h
@@ -0,0 +1,21 @@
+#pragma once
+
+class CEquip
+{
+public:
+	explicit CEquip();
+	~CEquip();
+
+
+private:
+
+
+
+public:
+	virtual void Initialize();
+	virtual void Update();
+	virtual void Render();
+	virtual void Release();
+
+};
+
