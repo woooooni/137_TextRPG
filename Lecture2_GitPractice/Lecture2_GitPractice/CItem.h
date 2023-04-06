c5c477f357995a5c9b9d66d6931b629d41793ac8
Tiny-Prince <55116786+woooooni@users.noreply.github.com>
2023-04-06 21:11:02 +0900

    Update CItem.h

--- Lecture2_GitPractice/Lecture2_GitPractice/CItem.h
+++ Lecture2_GitPractice/Lecture2_GitPractice/CItem.h
@@ -5,27 +5,21 @@
 class CItem
 {
 public:
-<<<<<<< .merge_file_cRxao7
 	explicit CItem();
-=======
 	explicit CItem() : iAmount(0) {}
->>>>>>> .merge_file_QQPLzM
 	virtual ~CItem();
 
 protected:
 	tagItem* m_tItem;
-<<<<<<< .merge_file_cRxao7
 
 public:
 	tagItem* GetItem() { return m_tItem; }
-=======
 	int iAmount;
 public:
 	tagItem* GetItem() { return m_tItem; }
 	int GetAmount() { return iAmount; }
 public:
 	void SetAmount(int _iAmount) { iAmount = _iAmount; }
->>>>>>> .merge_file_QQPLzM
 public:
 	void Render();
 
