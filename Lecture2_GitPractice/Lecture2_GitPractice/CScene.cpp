2b12f8818b57fb3d7519f1f8bc65b2130290a02e
Tiny-Prince <55116786+woooooni@users.noreply.github.com>
2023-04-06 21:13:30 +0900

    Update CScene.cpp

--- Lecture2_GitPractice/Lecture2_GitPractice/CScene.cpp
+++ Lecture2_GitPractice/Lecture2_GitPractice/CScene.cpp
@@ -1,14 +1,13 @@
 #include "stdafx.h"
 #include "CScene.h"
 
-<<<<<<< .merge_file_YishRK
 CScene::CScene()
 {
-=======
+}
 CScene::CScene(SCENE_TYPE _eType)
 {
 
->>>>>>> .merge_file_vzTopx
+
 }
 
 
