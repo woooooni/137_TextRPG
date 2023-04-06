cd5a1c9ae6de942b74ce8db4896bb3636a241070
Chan Yoon <psalms119165sin@gmail.com>
2023-04-06 16:41:26 +0900

    Feat "CObj 수정, CAttackObj 생성, CPlayer 수정"

---
+++ Lecture2_GitPractice/Lecture2_GitPractice/CAttackObj.cpp
@@ -0,0 +1,56 @@
+#include "stdafx.h"
+#include "CAttackObj.h"
+
+CAttackObj::CAttackObj()
+	: m_strName("")
+	, m_bDead(false)
+{
+	m_tStat = ATTACK_OBJ_STAT();
+}
+
+CAttackObj::CAttackObj(const string & _strName, const OBJECT_TYPE & _eObjType)
+	: m_strName("")
+	, m_bDead(false)
+	, CObj(_eObjType)
+{
+	m_tStat = ATTACK_OBJ_STAT();
+}
+
+CAttackObj::CAttackObj(const string & _strName, const ATTACK_OBJ_STAT & _tStat, const OBJECT_TYPE & _eObjType)
+	: m_strName("")
+	, m_bDead(false)
+	, m_tStat(_tStat)
+	, CObj(_eObjType)
+{
+}
+
+CAttackObj::~CAttackObj()
+{
+}
+
+const bool CAttackObj::Attack(CAttackObj * const _pAttackObj)
+{
+	if (nullptr == _pAttackObj) return false;
+
+	_pAttackObj->Damaged(m_tStat.m_iStr);
+
+	return true;
+}
+
+const bool CAttackObj::Damaged(const int _iDamage)
+{
+	if (0 > _iDamage) return false;
+
+	m_tStat.m_iCurHp -= _iDamage;
+
+	if (0 >= m_tStat.m_iCurHp)
+	{
+		if (Set_Dead)
+			return true;
+		else
+			return false;
+	}
+
+	return true;
+}
+
---
+++ Lecture2_GitPractice/Lecture2_GitPractice/CPlayer.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "CPlayer.h"
+
+
+CPlayer::CPlayer()
+{
+}
+
+
+CPlayer::~CPlayer()
+{
+}
+
+void CPlayer::Initialize()
+{
+}
+
+void CPlayer::Update()
+{
+}
+
+void CPlayer::Render()
+{
+}
+
+void CPlayer::Release()
+{
+}
