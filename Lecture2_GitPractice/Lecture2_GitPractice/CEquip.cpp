048356129a9ece5dadb1888b9d4fb488821ad585
Juns <gmlwnsgmlcks@gmail.com>
2023-04-07 10:53:48 +0900

    Feature "Equip progress"

--- Lecture2_GitPractice/Lecture2_GitPractice/CEquip.cpp
+++ Lecture2_GitPractice/Lecture2_GitPractice/CEquip.cpp
@@ -3,7 +3,7 @@
 #include "CItem.h"
 
 
-CEquip::CEquip() : m_pEqPlayer(nullptr), m_iter(m_mapEquip.begin())
+CEquip::CEquip() : m_pEqPlayer(nullptr), m_EqInven(nullptr), m_iter(m_mapEquip.begin())
 {
 }
 
@@ -24,15 +24,12 @@ void CEquip::Render()
 	if (m_mapEquip.empty())
 	{
 		cout << "현재 장착한 장비가 없습니다" << endl;
-		system("pause");
-		return;
 	}
-
 	else
 	{
-		for (auto iter : m_mapEquip)
+		for (m_iter = m_mapEquip.begin(); m_iter != m_mapEquip.end(); ++m_iter)
 		{
-			cout << "장비슬롯 : " << iter.first << '\t' << "이름 : " << iter.second->GetItem()->strName << endl;
+			cout << "장비슬롯 : " << m_iter->first << '\t' << "이름 : " << m_iter->second->GetItem()->strName << endl;
 			cout << "============================" << endl;
 		}
 	}
@@ -65,8 +62,19 @@ bool CEquip::Equip_Item(CItem* _pItem)
 
 void CEquip::Unequip_Item(CItem* _pItem)
 {
-  m_iter = m_mapEquip.find(_pItem->GetItem()->strName);
+  m_iter = m_mapEquip.find(_pItem->GetItem()->strDetailType);
   m_mapEquip.erase(m_iter);
   m_EqInven->AddItem(_pItem, 1);
   m_pEqPlayer->Reflect_Stat(_pItem, true);
+}
+
+void CEquip::Unequip_All()
+{
+	for (m_iter = m_mapEquip.begin(); m_iter != m_mapEquip.end(); ++m_iter)
+	{
+		m_EqInven->AddItem(m_iter->second, 1);
+		m_pEqPlayer->Reflect_Stat(m_iter->second, true);
+	}
+
+	m_mapEquip.clear();
 }
